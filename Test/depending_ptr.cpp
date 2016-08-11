25ddef8c1977cf0b02e1a250355f0a9c6f37777d
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-11 14:01:23 -0700

    Switch representation to a pointer and fix resulting breakage
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- Test/depending_ptr.cpp
+++ Test/depending_ptr.cpp
@@ -1,36 +1,30 @@
-// T must be a pointer type.
 template<typename T> class depending_ptr;
 
 template<typename T>
 class depending_ptr {
 public:
 	// Constructors
-	depending_ptr(T v);
-	depending_ptr(T& v);
+	depending_ptr(T *v);
 
 	// Unary operators
 	bool operator!(); // Prefix logical-not (is-NULL) operator
-	T* operator&(); // Prefix address-of operator
+	T** operator&(); // Prefix address-of operator
 	// No prefix bitwise complement operator (private)
-	T& operator*(); // Prefix indirection operator
-	T& operator++(); // Prefix increment operator
-	T operator++(int); // Postfix increment operator
+	T operator*(); // Prefix indirection operator
+	T* operator++(); // Prefix increment operator
+	T* operator++(int); // Postfix increment operator
 private:
 	int operator~(); // No prefix bitwise complement operator
-	T dp_rep;
+	int operator+(); // No prefix plus operator
+	int operator-(); // No prefix negation operator
+	T *dp_rep;
 };
 
 
 // Constructors
 
 template<typename T>
-depending_ptr<T>::depending_ptr(T v)
-{
-	this->dp_rep = v;
-}
-
-template<typename T>
-depending_ptr<T>::depending_ptr(T& v)
+depending_ptr<T>::depending_ptr(T *v)
 {
 	this->dp_rep = v;
 }
@@ -45,39 +39,49 @@ bool depending_ptr<T>::operator!() // Prefix logical-not (is-NULL) operator
 }
 
 template<typename T>
-T* depending_ptr<T>::operator&() // Prefix address-of operator
+T** depending_ptr<T>::operator&() // Prefix address-of operator
 {
-	T *temp = &this;
+	depending_ptr<T*> temp(&this->dp_rep);
 	return temp;
 }
 
 template<typename T>
-T& depending_ptr<T>::operator*() // Prefix indirection operator
+T depending_ptr<T>::operator*() // Prefix indirection operator
 {
 	return *this->dp_rep;
 }
 
 template<typename T>
-T& depending_ptr<T>::operator++() // Prefix increment operator
+T* depending_ptr<T>::operator++() // Prefix increment operator
 {
 	++this->dp_rep;
-	return *this;
+	return this->dp_rep;
 }
 
 template<typename T>
-T depending_ptr<T>::operator++(int) // Postfix increment operator
+T* depending_ptr<T>::operator++(int) // Postfix increment operator
 {
-	T temp = this->dp_rep;
+	T *temp = this->dp_rep;
 
 	++this->dp_rep;
 	return temp;
 }
 
+#include <iostream>
+
 int main(int argc, char **argv)
 {
 	char *cp = (char *)"abcde";
-	depending_ptr<char *> p(&cp[1]);
+	depending_ptr<char> p(&cp[1]);
 
-	stdout << *p;
+	std::cout << "Initialized value: ";
+	std::cout << *p;
+	std::cout << "\nPostfix ++: ";
+	p++;
+	std::cout << *p;
+	std::cout << "\nPrefix ++: ";
+	++p;
+	std::cout << *p;
+	std::cout << "\n";
 	return 0;
 }
