b1a763ab72a207ed69967a5566286a99f5ff9489
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-11 14:45:12 -0700

    Add a few more operators and tests
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- Test/depending_ptr.cpp
+++ Test/depending_ptr.cpp
@@ -13,10 +13,18 @@ public:
 	T operator*(); // Prefix indirection operator
 	T* operator++(); // Prefix increment operator
 	T* operator++(int); // Postfix increment operator
+	T* operator--(); // Prefix decrement operator
+	T* operator--(int); // Postfix decrement operator
+
+	// Binary relational operators
+	bool operator==(T *v);
+
 private:
+	// Disabled operators
 	int operator~(); // No prefix bitwise complement operator
 	int operator+(); // No prefix plus operator
 	int operator-(); // No prefix negation operator
+
 	T *dp_rep;
 };
 
@@ -41,8 +49,7 @@ bool depending_ptr<T>::operator!() // Prefix logical-not (is-NULL) operator
 template<typename T>
 T** depending_ptr<T>::operator&() // Prefix address-of operator
 {
-	depending_ptr<T*> temp(&this->dp_rep);
-	return temp;
+	return &this->dp_rep;
 }
 
 template<typename T>
@@ -67,12 +74,35 @@ T* depending_ptr<T>::operator++(int) // Postfix increment operator
 	return temp;
 }
 
+template<typename T>
+T* depending_ptr<T>::operator--() // Prefix decrement operator
+{
+	--this->dp_rep;
+	return this->dp_rep;
+}
+
+template<typename T>
+T* depending_ptr<T>::operator--(int) // Postfix decrement operator
+{
+	T *temp = this->dp_rep;
+
+	--this->dp_rep;
+	return temp;
+}
+
+template<typename T>
+bool depending_ptr<T>::operator==(T *v) // Infix equality relational operator
+{
+	return this->dp_rep == v; // BUGGY -- need external function
+}
+
 #include <iostream>
 
 int main(int argc, char **argv)
 {
 	char *cp = (char *)"abcde";
 	depending_ptr<char> p(&cp[1]);
+	depending_ptr<char> q((char *)nullptr);
 
 	std::cout << "Initialized value: ";
 	std::cout << *p;
@@ -82,6 +112,29 @@ int main(int argc, char **argv)
 	std::cout << "\nPrefix ++: ";
 	++p;
 	std::cout << *p;
+	std::cout << "\nPostfix --: ";
+	p--;
+	std::cout << *p;
+	std::cout << "\nPrefix --: ";
+	--p;
+	std::cout << *p;
+	std::cout << "\nPrefix &: ";
+	std::cout << &p;
+	std::cout << "\nPrefix *&: ";
+	std::cout << *&p;
+	std::cout << "\nPrefix **&: ";
+	std::cout << **&p;
+	std::cout << "\nPrefix !: ";
+	std::cout << !p;
+	std::cout << "\nPrefix !nullptr: ";
+	std::cout << !q;
+
+	// Binary operators
+	std::cout << "\nInfix ==: ";
+	std::cout << (p == nullptr);
+	// std::cout << " Again but equal: ";
+	// std::cout << (p == p);
+
 	std::cout << "\n";
 	return 0;
 }
