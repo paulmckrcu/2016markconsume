2c8fde41b25aca43bb5f87c3156d9edfa0df840a
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-12 15:31:26 -0700

    Add depending_ptr_inout
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- Test/test.sh
+++ Test/test.sh
@@ -1,9 +1,15 @@
 echo depending_ptr_simple
 g++ -std=c++11 -g -o depending_ptr_simple depending_ptr_simple.cpp -lpthread &&
 	./depending_ptr_simple
+
 echo depending_ptr_inparam
 g++ -std=c++11 -g -o depending_ptr_inparam depending_ptr_inparam.cpp -lpthread &&
 	./depending_ptr_inparam
+
 echo depending_ptr_outreturn
 g++ -std=c++11 -g -o depending_ptr_outreturn depending_ptr_outreturn.cpp -lpthread &&
 	./depending_ptr_outreturn
+
+echo depending_ptr_inout
+g++ -std=c++11 -g -o depending_ptr_inout depending_ptr_inout.cpp -lpthread &&
+	./depending_ptr_inout
