d378d20ef74c711a835d3e0f4ec4dff2e4498efc
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-12 15:36:22 -0700

    Add depending_ptr_fanout
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ Test/depending_ptr_fanout.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <assert.h>
+#include <pthread.h>
+#include "depending_ptr.hpp"
+#include "common.hpp"
+
+void *thread0(void *unused)
+{
+	struct rcutest *p;
+
+	p = (struct rcutest *)malloc(sizeof(*p));
+	assert(p);
+	p->a = 42;
+	rcu_store_release(&gp, p);
+	return NULL;
+}
+
+void thread1_help1(depending_ptr<struct rcutest> q)
+{
+	if (q)
+		assert(q->a == 42);
+}
+
+void thread1_help2(depending_ptr<struct rcutest> q)
+{
+	if (q)
+		assert(q->a != 43);
+}
+
+void *thread1(void *unused)
+{
+	depending_ptr<struct rcutest> p;
+
+	// std::this_thread::sleep_for(std::chrono::seconds(1));
+	p = rcu_consume(&gp);
+	thread1_help1(p);
+	thread1_help2(p);
+	return NULL;
+}
+
+int main(int argc, char **argv)
+{
+	pthread_t tid0;
+	pthread_t tid1;
+	struct rcutest *p;
+
+	if (pthread_create(&tid0, NULL, thread0, NULL)) {
+		perror("pthread_create(thread0)");
+		return(-1);
+	}
+	if (pthread_create(&tid1, NULL, thread1, NULL)) {
+		perror("pthread_create(thread1)");
+		return(-1);
+	}
+
+	if (pthread_join(tid0, NULL)) {
+		perror("pthread_join(tid0)");
+		return(-1);
+	}
+	if (pthread_join(tid1, NULL)) {
+		perror("pthread_join(tid1)");
+		return(-1);
+	}
+
+	return 0;
+}
--- Test/test.sh
+++ Test/test.sh
@@ -13,3 +13,7 @@ g++ -std=c++11 -g -o depending_ptr_outreturn depending_ptr_outreturn.cpp -lpthre
 echo depending_ptr_inout
 g++ -std=c++11 -g -o depending_ptr_inout depending_ptr_inout.cpp -lpthread &&
 	./depending_ptr_inout
+
+echo depending_ptr_fanout
+g++ -std=c++11 -g -o depending_ptr_fanout depending_ptr_fanout.cpp -lpthread &&
+	./depending_ptr_fanout
