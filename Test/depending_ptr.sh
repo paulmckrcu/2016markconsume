e5005904f3c2c6c484b55fcbc06eaea045a658a0
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-12 11:31:06 -0700

    Move testing to its own file
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- Test/depending_ptr.sh
+++
@@ -1,2 +0,0 @@
-g++ -std=c++11 -o depending_ptr depending_ptr.cpp pointer_cmp.cpp &&
-	./depending_ptr
---
+++ Test/test_depending_ptr.sh
@@ -0,0 +1,2 @@
+g++ -std=c++11 -o test_depending_ptr test_depending_ptr.cpp pointer_cmp.cpp &&
+	./test_depending_ptr
