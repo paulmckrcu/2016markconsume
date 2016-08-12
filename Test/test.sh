3f83ad6e7766e759d9f372261d871f6e29b01931
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-12 15:11:16 -0700

    Add input-parameter scenario
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ Test/test.sh
@@ -0,0 +1,6 @@
+echo depending_ptr_simple
+g++ -std=c++11 -g -o depending_ptr_simple depending_ptr_simple.cpp -lpthread &&
+	./depending_ptr_simple
+echo depending_ptr_inparam
+g++ -std=c++11 -g -o depending_ptr_inparam depending_ptr_inparam.cpp -lpthread &&
+	./depending_ptr_inparam
