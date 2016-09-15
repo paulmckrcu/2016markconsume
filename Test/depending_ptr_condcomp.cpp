5678820c4202e8b7514fc038b118265ece975b8f
JF Bastien <jfbastien@apple.com>
2016-09-14 22:33:23 -0700

    Auto-format everything
    I ran everything through: find . -name '*.*pp' -type f | xargs -n1 -I{} clang-format -style=WebKit -i {}

--- Test/depending_ptr_condcomp.cpp
+++ Test/depending_ptr_condcomp.cpp
@@ -1,106 +1,106 @@
-#include <iostream>
-#include <thread>
-#include <chrono>
-#include <assert.h>
-#include <pthread.h>
-#include "depending_ptr.hpp"
 #include "common.hpp"
+#include "depending_ptr.hpp"
+#include <assert.h>
+#include <chrono>
+#include <iostream>
+#include <pthread.h>
+#include <thread>
 
-void *thread0(void *unused)
+void* thread0(void* unused)
 {
-	rcutest *p;
-	rcutest1 *p1;
+    rcutest* p;
+    rcutest1* p1;
 
-	p = new rcutest();
-	assert(p);
-	p->a = 42;
-	p->b = 43;
-	rcu_store_release(&gp, p);
+    p = new rcutest();
+    assert(p);
+    p->a = 42;
+    p->b = 43;
+    rcu_store_release(&gp, p);
 
-	p1 = new rcutest1();
-	assert(p1);
-	p1->a = 41;
-	p1->rt.a = 42;
-	p1->rt.b = 43;
-	rcu_store_release(&g1p, p1);
+    p1 = new rcutest1();
+    assert(p1);
+    p1->a = 41;
+    p1->rt.a = 42;
+    p1->rt.b = 43;
+    rcu_store_release(&g1p, p1);
 
-	return nullptr;
+    return nullptr;
 }
 
 #ifdef FOO
 void thread1a_help(depending_ptr<rcutest> q)
 {
-	assert(q->a == 42);
+    assert(q->a == 42);
 }
 #endif
 
 void thread1b_help(depending_ptr<rcutest> q)
 {
-	assert(q->b == 43);
+    assert(q->b == 43);
 }
 
 void thread1_help(depending_ptr<rcutest> q)
 {
-	if (q) {
+    if (q) {
 #ifdef FOO
-		thread1a_help(q);
+        thread1a_help(q);
 #endif
-		thread1b_help(q);
-	}
+        thread1b_help(q);
+    }
 }
 
-void *thread1(void *unused)
+void* thread1(void* unused)
 {
-	depending_ptr<rcutest> p;
+    depending_ptr<rcutest> p;
 
-	// std::this_thread::sleep_for(std::chrono::seconds(1));
-	p = rcu_consume(&gp);
-	thread1_help(p);
-	return nullptr;
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    p = rcu_consume(&gp);
+    thread1_help(p);
+    return nullptr;
 }
 
-void *thread2(void *unused)
+void* thread2(void* unused)
 {
-	depending_ptr<rcutest1> p1;
+    depending_ptr<rcutest1> p1;
 
-	// std::this_thread::sleep_for(std::chrono::seconds(1));
-	p1 = rcu_consume(&g1p);
-	thread1_help(depending_ptr<rcutest>(&p1->rt));
-	return nullptr;
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    p1 = rcu_consume(&g1p);
+    thread1_help(depending_ptr<rcutest>(&p1->rt));
+    return nullptr;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	pthread_t tid0;
-	pthread_t tid1;
-	pthread_t tid2;
-	rcutest *p;
+    pthread_t tid0;
+    pthread_t tid1;
+    pthread_t tid2;
+    rcutest* p;
 
-	if (pthread_create(&tid0, nullptr, thread0, nullptr)) {
-		perror("pthread_create(thread0)");
-		return -1;
-	}
-	if (pthread_create(&tid1, nullptr, thread1, nullptr)) {
-		perror("pthread_create(thread1)");
-		return -1;
-	}
-	if (pthread_create(&tid2, nullptr, thread1, nullptr)) {
-		perror("pthread_create(thread2)");
-		return -1;
-	}
+    if (pthread_create(&tid0, nullptr, thread0, nullptr)) {
+        perror("pthread_create(thread0)");
+        return -1;
+    }
+    if (pthread_create(&tid1, nullptr, thread1, nullptr)) {
+        perror("pthread_create(thread1)");
+        return -1;
+    }
+    if (pthread_create(&tid2, nullptr, thread1, nullptr)) {
+        perror("pthread_create(thread2)");
+        return -1;
+    }
 
-	if (pthread_join(tid0, nullptr)) {
-		perror("pthread_join(tid0)");
-		return -1;
-	}
-	if (pthread_join(tid1, nullptr)) {
-		perror("pthread_join(tid1)");
-		return -1;
-	}
-	if (pthread_join(tid2, nullptr)) {
-		perror("pthread_join(tid2)");
-		return -1;
-	}
+    if (pthread_join(tid0, nullptr)) {
+        perror("pthread_join(tid0)");
+        return -1;
+    }
+    if (pthread_join(tid1, nullptr)) {
+        perror("pthread_join(tid1)");
+        return -1;
+    }
+    if (pthread_join(tid2, nullptr)) {
+        perror("pthread_join(tid2)");
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
--- Test/pointer_cmp.cpp
+++ Test/pointer_cmp.cpp
@@ -1,33 +1,32 @@
 namespace std {
 
-	bool pointer_cmp_eq_dep(void *p, void *q) noexcept
-	{
-		return p == q;
-	}
+bool pointer_cmp_eq_dep(void* p, void* q) noexcept
+{
+    return p == q;
+}
 
-	bool pointer_cmp_ne_dep(void *p, void *q) noexcept
-	{
-		return p != q;
-	}
+bool pointer_cmp_ne_dep(void* p, void* q) noexcept
+{
+    return p != q;
+}
 
-	bool pointer_cmp_gt_dep(void *p, void *q) noexcept
-	{
-		return p > q;
-	}
+bool pointer_cmp_gt_dep(void* p, void* q) noexcept
+{
+    return p > q;
+}
 
-	bool pointer_cmp_ge_dep(void *p, void *q) noexcept
-	{
-		return p >= q;
-	}
+bool pointer_cmp_ge_dep(void* p, void* q) noexcept
+{
+    return p >= q;
+}
 
-	bool pointer_cmp_lt_dep(void *p, void *q) noexcept
-	{
-		return p < q;
-	}
+bool pointer_cmp_lt_dep(void* p, void* q) noexcept
+{
+    return p < q;
+}
 
-	bool pointer_cmp_le_dep(void *p, void *q) noexcept
-	{
-		return p <= q;
-	}
-
+bool pointer_cmp_le_dep(void* p, void* q) noexcept
+{
+    return p <= q;
+}
 }
